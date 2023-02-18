17ee9f2dc02d6d373fc4f37f34236f6eae08468e
Yeongbae Jeon <ybjeon01@naver.com>
2023-02-17 18:48:31 -0700

    read csv file from file system

--- bank.py
+++ bank.py
@@ -1,40 +1,64 @@
 import socket
 import random
 from ipaddress import ip_address, IPv4Address
+import csv
+import json
+
+
+def read_csv_file(filepath):
+    data = []
+
+    with open(filepath) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                data.append(row)
+                line_count += 1
+    return data
+
 
 class Bank:
+
+    CUSTOMER_FILE_NAME = "customers.csv"
+
     BUFFER_SIZE = 1024
     IP = "0.0.0.0"
     PORT = 5000
-    
+
     customers = []      # a list of customers
     cohorts = []        # a list of all cohorts
 
     def __init__(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IP/UDP
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # IP/UDP
         sock.bind((Bank.IP, Bank.PORT))
 
         self.sock = sock
+        self.customers = read_csv_file(Bank.CUSTOMER_FILE_NAME)
 
     def run(self):
         while True:
             data, addr = self.sock.recvfrom(1024)
-            
-            if data.startswith("open"):
-                self.open(data, addr)
-            elif data.startswith("new-cohort"):
-                self.new_cohort(data, addr)
-            elif data.startswith("delete-cohort"):
-                self.delete_cohort(data, addr)
-            elif data.startswith("exit"):
-                self.exit(data, addr)
-            else:
-                # throw error or send error message
-                pass
+            response = None
 
+            if data.startswith("open"):
+                resonse = self.open(data, addr)
+            elif data.startswith("new-cohort"):
+                response = self.new_cohort(data, addr)
+            elif data.startswith("delete-cohort"):
+                response = self.delete_cohort(data, addr)
+            elif data.startswith("exit"):
+                response = self.exit(data, addr)
+            else:
+                response = {"res": "FAILURE"}
+
+            self.socket.sendto(json.dumps(response), addr)
 
     def open(self, data, addr):
-        tokens = data.split(' ')[1:]    # tokenize the command string, excluding the first parameter
+        # tokenize the command string, excluding the first parameter
+        tokens = data.split(' ')[1:]
 
         name = tokens[0]
         address = tokens[2]
@@ -43,14 +67,14 @@ class Bank:
 
         def validIP(address: str):
             return True if type(ip_address(address)) is IPv4Address else False
-            
+
         if (len(name) > 15 or (server_port == client_port) or validIP(address)):
             return {"res": "FAILURE"}
 
         # Add customer information to database
         if (tuple(tokens) not in Bank.customers):
             Bank.customers.append(tuple(tokens))
-            return {"res": "SUCCESS" }
+            return {"res": "SUCCESS"}
         else:
             return {"res": "FAILURE"}
 
@@ -63,8 +87,8 @@ class Bank:
 
         if (n > len(Bank.customers)):
             return {"res": "FAILURE"}
-        
-        new_group = [customer]  #new cohort group
+
+        new_group = [customer]  # new cohort group
         res = []
         customers_without_group = set()
 
@@ -73,14 +97,14 @@ class Bank:
             for group in self.cohorts:
                 if name not in group:       # empty intersection
                     customers_without_group.add(customer)
-        
 
         picked_customers = random.choice(customers_without_group, n-1)
 
         for customer in picked_customers:
             res.append(customer)
-            new_group.append(customer[0]) # add the name of the customer to the cohort group
-        
+            # add the name of the customer to the cohort group
+            new_group.append(customer[0])
+
         self.cohorts.append(new_group)
         return {
             "res": "SUCCESS",
@@ -92,3 +116,8 @@ class Bank:
 
     def exit(self, data, addr):
         pass
+
+
+if __name__ == "__main__":
+    bank = Bank()
+    bank.run()
