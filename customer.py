57ebed12e6855132169e4f0073d5c44c4c459836
dnguyen <dnguye92@asu.edu>
2023-03-13 20:26:03 -0700

    fix transfer, lost-transfer, initiate chkpt

--- customer.py
+++ customer.py
@@ -2,14 +2,61 @@ import socket
 import math
 import json
 import threading
+from threading import Lock
 
-class Checkpoint:
-
-    def __init__(self) -> None:
+class CheckpointAndRollback:
+    def __init__(self, myName, cohort) -> None:
+        self.labels = self.initialize_labels(myName, cohort)
         self.willing_to_checkpoint = True
-        self.last_recv_msg = {}
-        self.first_send_msg = {}
+        
+        #ready_to_roll = True
+        #willing_to_rollback = True
 
+    def initialize_labels(self, myName, cohort):
+        labels = {}
+        for each in cohort:
+            if each['name'] != myName:
+                other_client_name = each['name']
+                labels[other_client_name] = Label()
+        return labels
+    
+    def checkpoint(self, customer):
+        check_cohort = {}
+
+        for other_client_name, label in self.labels.items():
+            if label.last_recv > 0:
+                check_cohort[other_client_name] = label
+        
+
+        answers = []
+        for other_client_name in self.labels:
+            info = customer.cohort[other_client_name]
+
+            addr = info["ipv4"]
+            port2 = info["port2"]
+            ans = customer.send((addr, port2), "tentative")
+            answers.append(ans)
+        
+        for other_client_name in self.labels:
+            info = customer.cohort[other_client_name]
+
+            addr = info["ipv4"]
+            port2 = info["port2"]
+
+            if all(answers):
+                customer.send((addr, port2), "make-permanent")
+            else:
+                customer.send((addr, port2), "undo")
+
+    def checkpoint_recv(self, data):
+        pass
+
+
+class Label:
+    def __init__(self) -> None:
+        self.first_sent = 0
+        self.last_sent = 0
+        self.last_recv = 0
 
 class Customer:
 
@@ -32,6 +79,9 @@ class Customer:
         self.name = None
         self.balance = None
         self.cohort = None
+        self.labels = {}
+
+        self.balance_lock = Lock()
 
     def send(self, addr, msg):
         message = str.encode(msg)
@@ -54,10 +104,14 @@ class Customer:
 
             for each in self.cohort:
                 each["port2"] = int(each["port2"])
+                if each['name'] != self.name:
+                    other_client_name = each['name']
+                    self.labels[other_client_name] = Label()
+
             self.initialized = True
         return msg
 
-    def transfer(self, data):
+    def transfer(self, data, emulateLostTransfer=False):
         tokens = data.split()
 
         command = tokens[0]
@@ -67,7 +121,7 @@ class Customer:
 
         if not self.initialized:
             return {"res": "FAILURE", "reason": "not initialized"}
-        
+
         if not self.cohort:
             return {"res": "FAILURE", "reason": "no cohort"}
 
@@ -79,9 +133,15 @@ class Customer:
                 msg = self.withdraw(amount)
                 if msg["res"] == "FAILURE":
                     return msg
-                return self.send((ipv4, port2), data)
+
+                self.labels[recipient].last_sent = label
+
+                if not emulateLostTransfer:
+                    data += f" {self.name}"
+                    return self.send((ipv4, port2), data)
+
         return {"res": "FAILURE", "reason": "recipient not found"}
-    
+
     def listen_to_cohort(self):
         def helper():
             ipv4 = None
@@ -92,10 +152,10 @@ class Customer:
                     ipv4 = each['ipv4']
                     port2 = each['port2']
                     break
-                        
+
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # IP/UDP
             sock.bind((ipv4, port2))
-            
+
             while True:
                 data, addr = sock.recvfrom(Customer.BUFFER_SIZE)
                 data = data.decode()
@@ -112,22 +172,25 @@ class Customer:
 
                 # later save checkpoint to a file
                 self.sock.sendto(json.dumps(response).encode(), addr)
-            
-        
+
         if not self.initialized:
             return {"res": "FAILURE", "reason": "not initialized"}
-        
+
         extra_thread = threading.Thread(target=helper)
         extra_thread.start()
-        
 
     def transfer_recv(self, data, addr):
         tokens = data.split()
         command = tokens[0]
         amount = int(tokens[1])
+        recipient = tokens[2]
+        label = int(tokens[3])
+        sender = tokens[4]
 
         msg = self.deposit(amount)
+
         if msg["res"] == "SUCCESS":
+            self.labels[sender].last_recv += 1
             del msg['balance']
         return msg
 
@@ -137,7 +200,8 @@ class Customer:
         if amount <= 0:
             return {"res": "FAILURE", "reason": "amount is less than or equal to zero"}
 
-        self.balance += amount
+        with self.balance_lock:
+            self.balance += amount
         return {"res": "SUCCESS", "balance": self.balance}
 
     def withdraw(self, amount):
@@ -145,11 +209,12 @@ class Customer:
             return {"res": "FAILURE", "reason": "not initialized"}
         if amount <= 0:
             return {"res": "FAILURE", "reason": "amount is less than or equal to zero"}
+        with self.balance_lock:
+            self.balance -= amount
+            if (self.balance < 0):
+                self.balance += amount
+                return {"res": "FAILURE", "reason": "account balance negative"}
 
-        self.balance -= amount
-        if (self.balance < 0):
-            self.balance += amount
-            return {"res": "FAILURE", "reason": "account balance negative"}
         return {"res": "SUCCESS", "balance": self.balance}
 
 
@@ -159,7 +224,7 @@ if __name__ == "__main__":
     while True:
         command = input("msg: ")
         msg = None
-        #### iniate threads for peer to peer communication
+        # iniate threads for peer to peer communication
         if command.startswith("get"):
             msg = customer.get(command)
         elif command.startswith("listen-to-cohort"):
@@ -174,8 +239,10 @@ if __name__ == "__main__":
             msg = customer.withdraw(amount)
         elif command.startswith("transfer"):
             msg = customer.transfer(command)
+        elif command.startswith("lost-transfer"):
+            msg = customer.transfer(command, emulateLostTransfer=True)
         else:
             msg = customer.send(Customer.SERVER_ADDR, command)
-        
+
         if msg is not None:
             print(msg)
